32fe024bfa6f2461515202e7f0945de4878aeace
Stepland <16676308+Stepland@users.noreply.github.com>
2020-02-08 16:00:47 +0100

    Remove test slowdown

--- src/Resources/Autoloader.cpp
+++ src/Resources/Autoloader.cpp
@@ -5,62 +5,63 @@
 #include <mutex>
 #include <thread>
 
-void Textures::Autoloader::load(const fs::path& path) {
-    if (has(path) or is_loading(path)) {
-        return;
-    }
-    {
-        std::unique_lock lock{m_is_loading_mutex};
-        m_is_loading.insert(path);
-    }
-    auto texture = std::make_shared<sf::Texture>();
-    if (!texture->loadFromFile(path)) {
-        throw std::invalid_argument("Unable to load cover image : "+path.string());
-    }
-    texture->setSmooth(true);
-    std::this_thread::sleep_for(std::chrono::seconds(2));
-    {
-        std::unique_lock<std::shared_mutex> lock_mapping{m_mapping_mutex, std::defer_lock};
-        std::unique_lock<std::shared_mutex> lock_is_loading{m_is_loading_mutex, std::defer_lock};
-        std::lock(lock_mapping, lock_is_loading);
-        m_mapping.emplace(path, texture);
-        m_is_loading.erase(path);
-    }
-    std::cout << "Loaded " << path << std::endl;
-}
-
-void Textures::Autoloader::async_load(const fs::path& path) {
-    std::thread t(&Textures::Autoloader::load, this, path);
-    t.detach();
-}
-
-std::optional<std::shared_ptr<sf::Texture>> Textures::Autoloader::async_get(const fs::path& path) {
-    if (not has(path)) {
-        if (not is_loading(path)) {
-            async_load(path);
+namespace Textures {
+    void Autoloader::load(const fs::path& path) {
+        if (has(path) or is_loading(path)) {
+            return;
         }
-        return {};
-    } else {
-        return get(path);
+        {
+            std::unique_lock lock{m_is_loading_mutex};
+            m_is_loading.insert(path);
+        }
+        auto texture = std::make_shared<sf::Texture>();
+        if (!texture->loadFromFile(path)) {
+            throw std::invalid_argument("Unable to load cover image : "+path.string());
+        }
+        texture->setSmooth(true);
+        {
+            std::unique_lock<std::shared_mutex> lock_mapping{m_mapping_mutex, std::defer_lock};
+            std::unique_lock<std::shared_mutex> lock_is_loading{m_is_loading_mutex, std::defer_lock};
+            std::lock(lock_mapping, lock_is_loading);
+            m_mapping.emplace(path, texture);
+            m_is_loading.erase(path);
+        }
+        std::cout << "Loaded " << path << std::endl;
     }
-}
 
-std::optional<std::shared_ptr<sf::Texture>> Textures::Autoloader::get(const fs::path& path) {
-    std::shared_lock lock{m_mapping_mutex};
-    if (has(path)) {
-        return m_mapping.at(path);
-    } else {
-        return {};
+    void Autoloader::async_load(const fs::path& path) {
+        std::thread t(&Autoloader::load, this, path);
+        t.detach();
     }
-}
+
+    std::optional<AutoloadedTexture> Autoloader::async_get(const fs::path& path) {
+        if (not has(path)) {
+            if (not is_loading(path)) {
+                async_load(path);
+            }
+            return {};
+        } else {
+            return get(path);
+        }
+    }
+
+    std::optional<AutoloadedTexture> Autoloader::get(const fs::path& path) {
+        std::shared_lock lock{m_mapping_mutex};
+        if (has(path)) {
+            return m_mapping.at(path);
+        } else {
+            return {};
+        }
+    }
 
 
-bool Textures::Autoloader::has(const fs::path& path) {
-    std::shared_lock lock{m_mapping_mutex};
-    return m_mapping.find(path) != m_mapping.end();
-}
+    bool Autoloader::has(const fs::path& path) {
+        std::shared_lock lock{m_mapping_mutex};
+        return m_mapping.find(path) != m_mapping.end();
+    }
 
-bool Textures::Autoloader::is_loading(const fs::path& path) {
-    std::shared_lock lock{m_is_loading_mutex};
-    return m_is_loading.find(path) != m_is_loading.end();
+    bool Autoloader::is_loading(const fs::path& path) {
+        std::shared_lock lock{m_is_loading_mutex};
+        return m_is_loading.find(path) != m_is_loading.end();
+    }
 }
--- src/Resources/Autoloader.hpp
+++ src/Resources/Autoloader.hpp
@@ -5,10 +5,12 @@
 #include <optional>
 #include <shared_mutex>
 #include <string>
+#include <tuple>
 #include <unordered_map>
 #include <unordered_set>
 
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
 namespace fs = std::filesystem;
 
@@ -23,18 +25,26 @@ namespace std {
 }
 
 namespace Textures {
+
+    // Hold time elapsed since loaded
+    struct AutoloadedTexture {
+        AutoloadedTexture(std::shared_ptr<sf::Texture> t_texture) : texture(t_texture), clock() {};
+        std::shared_ptr<sf::Texture> texture;
+        sf::Clock clock;
+    };
+
     // Loads textures asynchronously (in the background) on demand and stores them in a map for easy path-based access
     class Autoloader {
     public:
         Autoloader() = default;
-        std::optional<std::shared_ptr<sf::Texture>> async_get(const fs::path& path);
-        std::optional<std::shared_ptr<sf::Texture>> get(const fs::path& path);
+        std::optional<AutoloadedTexture> async_get(const fs::path& path);
+        std::optional<AutoloadedTexture> get(const fs::path& path);
         void load(const fs::path& path);
         void async_load(const fs::path& path);
         bool has(const fs::path& path);
         bool is_loading(const fs::path& path);
     private:
-        std::unordered_map<fs::path, std::shared_ptr<sf::Texture>> m_mapping;
+        std::unordered_map<fs::path, AutoloadedTexture> m_mapping;
         std::shared_mutex m_mapping_mutex;
         std::unordered_set<fs::path> m_is_loading;
         std::shared_mutex m_is_loading_mutex;
