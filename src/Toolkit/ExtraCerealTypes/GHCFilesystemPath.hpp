4ed188ea706d7b71bf2150b72b4a6826e22399d1
Stepland <16676308+Stepland@users.noreply.github.com>
2020-03-02 13:45:25 +0100

    OMFG kill me

--- src/Toolkit/ExtraCerealTypes/GHCFilesystemPath.hpp
+++ src/Toolkit/ExtraCerealTypes/GHCFilesystemPath.hpp
@@ -2,16 +2,19 @@
 
 #include <string>
 
-#include <cereal/details/traits.hpp>
+#include <cereal/cereal.hpp>
 #include <ghc/filesystem.hpp>
 
+namespace ghc {
+    namespace filesystem {
+        template <class Archive>
+        std::string save_minimal(const Archive &, const path& p) {
+            return p.string();
+        }
 
-template <class Archive>
-std::string save_minimal(const Archive &, ghc::filesystem::path& p) {
-    return p.string();
-}
-
-template <class Archive>
-void load_minimal(const Archive &, ghc::filesystem::path& p, const std::string& value) {
-    p = ghc::filesystem::path{value};
+        template <class Archive>
+        void load_minimal(const Archive &, path& p, const std::string& value) {
+            p = path{value};
+        }
+    }
 }
