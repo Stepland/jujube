dfdd2390e8ecd606c4c7a591bb3ca945ba497159
Stepland <16676308+Stepland@users.noreply.github.com>
2020-03-07 01:05:36 +0100

    Add useful comments

--- src/Screens/MusicSelect/PanelLayout.hpp
+++ src/Screens/MusicSelect/PanelLayout.hpp
@@ -10,12 +10,16 @@
 namespace MusicSelect {
 
     class Panel;
-    // PanelLayout restricts the ways you can create a scrollable grid of panels
+    // PanelLayout restricts the ways you can create a scrollable grid of panels usable in a Ribbon
     class PanelLayout : public std::vector<std::array<std::shared_ptr<Panel>,3>> {
     public:
+        // Takes of map of category name and associated Panels, useful for all the sorted layouts
         explicit PanelLayout(const std::map<std::string,std::vector<std::shared_ptr<Panel>>>& categories, SharedResources& resources);
+        // Arranges all the panels in the vector in columns of three
         explicit PanelLayout(const std::vector<std::shared_ptr<Panel>>& panels, SharedResources& resources);
+        // Stepmania-like empty layout with big red panels that say EMPTY
         static PanelLayout red_empty_layout(SharedResources& resources);
+        // Standard title sort with categories for each letter
         static PanelLayout title_sort(const Data::SongList& song_list, SharedResources& resources);
     private:
         void fill_layout(SharedResources& resources);
