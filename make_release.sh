48324f39ce30839f38d3f946039e99cc13cf4fd2
Seth <seth@ske.moe>
2020-07-19 20:41:53 -0500

    uses copy_dependencies python script now for making a release archive

--- make_release.sh
+++ make_release.sh
@@ -5,7 +5,7 @@ meson configure build -Ddebug=false -Doptimization=2
 cd build
 ninja
 cp ../assets assets -r
-cp -t . /mingw64/bin/libbrotlidec.dll /mingw64/bin/libharfbuzz-0.dll /mingw64/bin/libpng16-16.dll /mingw64/bin/libvorbis-0.dll /mingw64/bin/zlib1.dll /mingw64/bin/libbz2-1.dll /mingw64/bin/libiconv-2.dll /mingw64/bin/libsfml-audio-2.dll /mingw64/bin/libvorbisenc-2.dll /mingw64/bin/libfreetype-6.dll /mingw64/bin/libintl-8.dll /mingw64/bin/libsfml-graphics-2.dll /mingw64/bin/libvorbisfile-3.dll /mingw64/bin/libgcc_s_seh-1.dll /mingw64/bin/libogg-0.dll /mingw64/bin/libsfml-system-2.dll /mingw64/bin/libwinpthread-1.dll /mingw64/bin/libFLAC-8.dll /mingw64/bin/libglib-2.0-0.dll /mingw64/bin/libopenal-1.dll /mingw64/bin/libsfml-window-2.dll /mingw64/bin/libbrotlicommon.dll /mingw64/bin/libgraphite2.dll /mingw64/bin/libpcre-1.dll /mingw64/bin/libstdc++-6.dll
+../utils/copy_dependencies.py -f jujube.exe
 
 rm -rf jujube@exe meson* build.ninja .ninja* compile_commands.json test
 
